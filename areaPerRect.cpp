f215d5f89523f20bbb2c5b020434239466bfe7c5
Joanna Keza <joanna.keza@stu.ocsb.ca>
2025-02-18 18:54:08 +0000

    Added

---
+++ areaPerRect.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// Copyright (c) 2025 Joanna Keza All rights reserved.
+//
+// Created by: Joanna Keza
+// Date: February 18, 2025
+// This program calculates the area and perimeter of a 2cm x 10cm rectangle.
+
+#include <iostream>
+
+int main() {
+    std::cout << "For a rectangle with a length of 2cm and width of 10cm: \n";
+    std::cout << "\n";
+    std::cout << "Area =  " << 2 * 10 << " cm^2\n";
+    std::cout << "Perimeter =  " << 2 * (2 + 10) << " cm\n";
+}
